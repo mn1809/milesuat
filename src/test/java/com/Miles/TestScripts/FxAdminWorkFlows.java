dfee20561efd7cb29af785299580e9db042b7af5
Manoj <manoj.hr@mileseducation.com>
2024-11-04 18:04:50 +0530

    Checking Jenkins1

--- src/test/java/com/Miles/TestScripts/FxAdminWorkFlows.java
+++ src/test/java/com/Miles/TestScripts/FxAdminWorkFlows.java
@@ -1,234 +1,234 @@
-package com.Miles.TestScripts;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.miles.BaseSettings.MilesSettings;
-import com.miles.PageLibRepo.AdminPageLib;
-import com.miles.PageLibRepo.HomePageLib;
-import com.miles.PageLibRepo.LoginPageLib;
-import com.miles.Utilities.MilesUtilities;
-
-public class FxAdminWorkFlows extends MilesSettings
-{	
-	 WebDriver driver = null ;
-	 LoginPageLib login ;
-	 HomePageLib homeObj ;
-	 
-	 AdminPageLib adminLib;
-	 String expectedInfoTxt = "User's information updated successfully." ;
-	 String ClassName = this.getClass().getSimpleName().toString();
-	 
-	 @Parameters({ "enivironment" })
-	 @BeforeMethod
-    private void Initialize(String env)
-    {
-	 this.driver = DecideEnvironment(env);
-	 login = new LoginPageLib(driver);
-	 homeObj = login.login("admin@fourthfrontier.com", MilesUtilities.DecryptPass("YXV0b21hdGlvbjRm"));
-	
-    }
-	 
-	 @AfterMethod
-	 private void CloseDriverSession(ITestResult result) throws Exception
-	 {
-		 if (result.getStatus() == ITestResult.FAILURE) 
-		 {
-			 MilesUtilities.createWorkFlowFolder(ClassName);
-			 MilesUtilities.CaptureEvidance(driver,result.getMethod().getMethodName(),result,ClassName);
-			 System.out.println(result.getMethod().getMethodName()+" Test Failed Due to the reason\n"+result.getThrowable().getMessage());
-		 }        
-		 driver.quit();
-	 }
-	
-	 
-@BeforeClass
-private void SetEvidenceDir()
-{
-	MilesUtilities.createDateBasedDirectory();
-	
-}
-	  
-		@Test (description = "Purpose of this test is to Verify that admin can change the User Level and enable Arrhythmia and that change will remain intact")
-		public void VerifyUserLevelChangesAreSaved()
-		{
-				ChangeUserSettings(homeObj);
-				// Log out after changes and log in back
-				homeObj.logout();
-				homeObj = login.login("admin@fourthfrontier.com",MilesUtilities.DecryptPass("YXV0b21hdGlvbjRm"));
-				
-				VerifyChangedUserSettingsAreStillIntact(homeObj);
-			
-				justSleepFor(3);
-			// Revert the user back to non premium user for next run
-				RevertChangesForNextRun(homeObj);
-		}
-		
-		@Test (description = "Purpose of this test is to Verify admin can search any user in our system")
-		public void VerifyAdminSearch()
-		{			
-					justSleepFor(3);
-					ClickOnAdminMenu();
-					justSleepFor(1);
-					clickOnAdminPanel();
-					justSleepFor(4);
-					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
-					justSleepFor(2);
-					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");		
-		 }
-		
-		@Test (description = "Purpose of this test is to Verify admin can search any user in our system")
-		public void VerifyUserLevelsAvailable()
-		{			
-					justSleepFor(3);
-					ClickOnAdminMenu();
-					justSleepFor(1);
-					clickOnAdminPanel();
-					justSleepFor(4);
-					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
-					justSleepFor(2);
-					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
-					
-		 }
-		
-		@Test (description = "Purpose of this test is to Verify that current user's name is displayed when admin access any user's dashboard")
-		public void VerifyUserNameDisplayedOnDashboard()
-		{			
-					String UserName = "Ravikiran";
-					justSleepFor(3);
-					ClickOnAdminMenu();
-					justSleepFor(1);
-					clickOnAdminPanel();
-					justSleepFor(4);
-					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
-					justSleepFor(2);
-					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
-					adminLib.SelectDisplyedUser();
-					justSleepFor(6);
-					homeObj.VerifyUserNameIsDisplayedForUser(UserName);			
-		 }
-		
-		 @Test(description = "Purpose of this test is to Verify Levels of users in our system")
-			public void VerifyUserLevelsDropDown() throws InterruptedException 
-			{		
-				justSleepFor(3);
-				ClickOnAdminMenu();
-				justSleepFor(1);
-				clickOnAdminPanel();
-				justSleepFor(4);
-				homeObj.SearchUsers("ravikiran@fourthfrontier.com");
-				justSleepFor(2);
-				adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
-				homeObj.VerifyUserLevelOptions();
-			}
-		 
-	
-			
-			@Test(description = "Purpose of this test is to Verify if Admin can login in")
-			public void VerifyAdminMenuOptions_AdminUser() throws Exception
-			{		
-							justSleepFor(4);
-							ClickOnAdminMenu();
-							justSleepFor(2);
-							VerifyAdminMenuOptions();
-							ClickOnAdminMenu(); // Close the menu 
-							
-			}
-		 
-		
-		@Test(description = "Purpose of this test is to Verify if Admin can login in")
-		public void VerifyLoggedInUser_AdminUser() throws Exception
-		{		
-						justSleepFor(4);
-						homeObj.VerifyLoggedInUserName(UserLevel.Admin.toString());
-		}
-/*
- * 
- * 
- * Healper Methods
- */
-	 public void ChangeUserSettings(HomePageLib hp)
-		{
-			
-			justSleepFor(3);
-			ClickOnAdminMenu();
-			justSleepFor(1);
-			clickOnAdminPanel();
-			justSleepFor(4);
-			adminLib = new AdminPageLib(driver);
-			hp.SearchUsers("ravikiran@fourthfrontier.com");
-			justSleepFor(2);
-			adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
-			justSleepFor(4);
-			hp.SelectUserLevel(1);
-			justSleepFor(3);
-			hp.GetNotificationText();
-			hp.VerifyUserInfoPopUp(expectedInfoTxt);
-			hp.EnableArrhytmia();
-			justSleepFor(3);
-			Assert.assertTrue(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Enabled");
-		}
-		public void VerifyChangedUserSettingsAreStillIntact(HomePageLib hp)
-		{
-			ClickOnAdminMenu();
-			justSleepFor(1);
-			clickOnAdminPanel();
-			justSleepFor(4);
-			adminLib = new AdminPageLib(driver);
-			hp.SearchUsers("ravikiran@fourthfrontier.com");
-			justSleepFor(2);
-			//smartWait()
-			adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
-			hp.VerifyUserLevel("Premium User");
-			Assert.assertTrue(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Enabled");
-		}
-		public void RevertChangesForNextRun(HomePageLib hp)
-		{
-			justSleepFor(2);
-			hp.SelectUserLevel(0);
-			hp.VerifyUserInfoPopUp(expectedInfoTxt);
-			hp.DisableArrhytmia();
-			Assert.assertFalse(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Disabled");
-			justSleepFor(5);
-		}
-		
-		/*
-		 * Helper Methods
-		 */
-		
-		
-		public void ClickOnAdminMenu()
-		{
-			driver.findElement(By.className("sidebar-toggle")).click();
-			
-		}
-		
-		public void clickOnAdminPanel()
-		{
-			List <WebElement> AdminOptions = driver.findElements(By.className("listbrdr"));
-			
-			//WebElement MyDasboard = AdminOptions.get(0);
-			 AdminOptions.get(1).click();
-			
-			//WebElement LiveUserDashBoard = AdminOptions.get(2);
-		}
-		
-		public void VerifyAdminMenuOptions()
-		{
-			List <WebElement> AdminOptions = driver.findElements(By.className("listbrdr"));
-			
-			Assert.assertEquals(AdminOptions.get(0).getText(), "My Dashboard");
-			Assert.assertEquals(AdminOptions.get(1).getText(), "Admin Panel");
-			Assert.assertEquals(AdminOptions.get(2).getText(), "Live User Dashboard");
-		}
-		
-}
+//package com.Miles.TestScripts;
+//import java.util.List;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.testng.Assert;
+//import org.testng.ITestResult;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Parameters;
+//import org.testng.annotations.Test;
+//
+//import com.miles.BaseSettings.MilesSettings;
+//import com.miles.PageLibRepo.AdminPageLib;
+//import com.miles.PageLibRepo.HomePageLib;
+//import com.miles.PageLibRepo.LoginPageLib;
+//import com.miles.Utilities.MilesUtilities;
+//
+//public class FxAdminWorkFlows extends MilesSettings
+//{	
+//	 WebDriver driver = null ;
+//	 LoginPageLib login ;
+//	 HomePageLib homeObj ;
+//	 
+//	 AdminPageLib adminLib;
+//	 String expectedInfoTxt = "User's information updated successfully." ;
+//	 String ClassName = this.getClass().getSimpleName().toString();
+//	 
+//	 @Parameters({ "enivironment" })
+//	 @BeforeMethod
+//    private void Initialize(String env)
+//    {
+//	 this.driver = DecideEnvironment(env);
+//	 login = new LoginPageLib(driver);
+//	 homeObj = login.login("admin@fourthfrontier.com", MilesUtilities.DecryptPass("YXV0b21hdGlvbjRm"));
+//	
+//    }
+//	 
+//	 @AfterMethod
+//	 private void CloseDriverSession(ITestResult result) throws Exception
+//	 {
+//		 if (result.getStatus() == ITestResult.FAILURE) 
+//		 {
+//			 MilesUtilities.createWorkFlowFolder(ClassName);
+//			 MilesUtilities.CaptureEvidance(driver,result.getMethod().getMethodName(),result,ClassName);
+//			 System.out.println(result.getMethod().getMethodName()+" Test Failed Due to the reason\n"+result.getThrowable().getMessage());
+//		 }        
+//		 driver.quit();
+//	 }
+//	
+//	 
+//@BeforeClass
+//private void SetEvidenceDir()
+//{
+//	MilesUtilities.createDateBasedDirectory();
+//	
+//}
+//	  
+//		@Test (description = "Purpose of this test is to Verify that admin can change the User Level and enable Arrhythmia and that change will remain intact")
+//		public void VerifyUserLevelChangesAreSaved()
+//		{
+//				ChangeUserSettings(homeObj);
+//				// Log out after changes and log in back
+//				homeObj.logout();
+//				homeObj = login.login("admin@fourthfrontier.com",MilesUtilities.DecryptPass("YXV0b21hdGlvbjRm"));
+//				
+//				VerifyChangedUserSettingsAreStillIntact(homeObj);
+//			
+//				justSleepFor(3);
+//			// Revert the user back to non premium user for next run
+//				RevertChangesForNextRun(homeObj);
+//		}
+//		
+//		@Test (description = "Purpose of this test is to Verify admin can search any user in our system")
+//		public void VerifyAdminSearch()
+//		{			
+//					justSleepFor(3);
+//					ClickOnAdminMenu();
+//					justSleepFor(1);
+//					clickOnAdminPanel();
+//					justSleepFor(4);
+//					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
+//					justSleepFor(2);
+//					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");		
+//		 }
+//		
+//		@Test (description = "Purpose of this test is to Verify admin can search any user in our system")
+//		public void VerifyUserLevelsAvailable()
+//		{			
+//					justSleepFor(3);
+//					ClickOnAdminMenu();
+//					justSleepFor(1);
+//					clickOnAdminPanel();
+//					justSleepFor(4);
+//					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
+//					justSleepFor(2);
+//					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
+//					
+//		 }
+//		
+//		@Test (description = "Purpose of this test is to Verify that current user's name is displayed when admin access any user's dashboard")
+//		public void VerifyUserNameDisplayedOnDashboard()
+//		{			
+//					String UserName = "Ravikiran";
+//					justSleepFor(3);
+//					ClickOnAdminMenu();
+//					justSleepFor(1);
+//					clickOnAdminPanel();
+//					justSleepFor(4);
+//					homeObj.SearchUsers("ravikiran@fourthfrontier.com");
+//					justSleepFor(2);
+//					adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
+//					adminLib.SelectDisplyedUser();
+//					justSleepFor(6);
+//					homeObj.VerifyUserNameIsDisplayedForUser(UserName);			
+//		 }
+//		
+//		 @Test(description = "Purpose of this test is to Verify Levels of users in our system")
+//			public void VerifyUserLevelsDropDown() throws InterruptedException 
+//			{		
+//				justSleepFor(3);
+//				ClickOnAdminMenu();
+//				justSleepFor(1);
+//				clickOnAdminPanel();
+//				justSleepFor(4);
+//				homeObj.SearchUsers("ravikiran@fourthfrontier.com");
+//				justSleepFor(2);
+//				adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
+//				homeObj.VerifyUserLevelOptions();
+//			}
+//		 
+//	
+//			
+//			@Test(description = "Purpose of this test is to Verify if Admin can login in")
+//			public void VerifyAdminMenuOptions_AdminUser() throws Exception
+//			{		
+//							justSleepFor(4);
+//							ClickOnAdminMenu();
+//							justSleepFor(2);
+//							VerifyAdminMenuOptions();
+//							ClickOnAdminMenu(); // Close the menu 
+//							
+//			}
+//		 
+//		
+//		@Test(description = "Purpose of this test is to Verify if Admin can login in")
+//		public void VerifyLoggedInUser_AdminUser() throws Exception
+//		{		
+//						justSleepFor(4);
+//						homeObj.VerifyLoggedInUserName(UserLevel.Admin.toString());
+//		}
+///*
+// * 
+// * 
+// * Healper Methods
+// */
+//	 public void ChangeUserSettings(HomePageLib hp)
+//		{
+//			
+//			justSleepFor(3);
+//			ClickOnAdminMenu();
+//			justSleepFor(1);
+//			clickOnAdminPanel();
+//			justSleepFor(4);
+//			adminLib = new AdminPageLib(driver);
+//			hp.SearchUsers("ravikiran@fourthfrontier.com");
+//			justSleepFor(2);
+//			adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
+//			justSleepFor(4);
+//			hp.SelectUserLevel(1);
+//			justSleepFor(3);
+//			hp.GetNotificationText();
+//			hp.VerifyUserInfoPopUp(expectedInfoTxt);
+//			hp.EnableArrhytmia();
+//			justSleepFor(3);
+//			Assert.assertTrue(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Enabled");
+//		}
+//		public void VerifyChangedUserSettingsAreStillIntact(HomePageLib hp)
+//		{
+//			ClickOnAdminMenu();
+//			justSleepFor(1);
+//			clickOnAdminPanel();
+//			justSleepFor(4);
+//			adminLib = new AdminPageLib(driver);
+//			hp.SearchUsers("ravikiran@fourthfrontier.com");
+//			justSleepFor(2);
+//			//smartWait()
+//			adminLib.VerifyIfSearchedUserAppeared("ravikiran@fourthfrontier.com");
+//			hp.VerifyUserLevel("Premium User");
+//			Assert.assertTrue(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Enabled");
+//		}
+//		public void RevertChangesForNextRun(HomePageLib hp)
+//		{
+//			justSleepFor(2);
+//			hp.SelectUserLevel(0);
+//			hp.VerifyUserInfoPopUp(expectedInfoTxt);
+//			hp.DisableArrhytmia();
+//			Assert.assertFalse(hp.CheckIfArrhytmiaIsEnabled(), "Arrhytmia is Disabled");
+//			justSleepFor(5);
+//		}
+//		
+//		/*
+//		 * Helper Methods
+//		 */
+//		
+//		
+//		public void ClickOnAdminMenu()
+//		{
+//			driver.findElement(By.className("sidebar-toggle")).click();
+//			
+//		}
+//		
+//		public void clickOnAdminPanel()
+//		{
+//			List <WebElement> AdminOptions = driver.findElements(By.className("listbrdr"));
+//			
+//			//WebElement MyDasboard = AdminOptions.get(0);
+//			 AdminOptions.get(1).click();
+//			
+//			//WebElement LiveUserDashBoard = AdminOptions.get(2);
+//		}
+//		
+//		public void VerifyAdminMenuOptions()
+//		{
+//			List <WebElement> AdminOptions = driver.findElements(By.className("listbrdr"));
+//			
+//			Assert.assertEquals(AdminOptions.get(0).getText(), "My Dashboard");
+//			Assert.assertEquals(AdminOptions.get(1).getText(), "Admin Panel");
+//			Assert.assertEquals(AdminOptions.get(2).getText(), "Live User Dashboard");
+//		}
+//		
+//}
--- src/test/java/com/Miles/TestScripts/FxUserWorkFlows.java
+++ src/test/java/com/Miles/TestScripts/FxUserWorkFlows.java
@@ -1,157 +1,157 @@
-package com.Miles.TestScripts;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.miles.BaseSettings.MilesSettings;
-import com.miles.PageLibRepo.HeartHealthPageLib;
-import com.miles.PageLibRepo.HomePageLib;
-import com.miles.PageLibRepo.LoginPageLib;
-import com.miles.PageLibRepo.WorkoutDetailsPageLib;
-import com.miles.Utilities.MilesUtilities;
-
-public class FxUserWorkFlows extends MilesSettings 
-{	
-	 WebDriver driver = null ;
-	 LoginPageLib loginObj ;
-	 HomePageLib homeObj ;
-	 
-	 String ClassName = this.getClass().getSimpleName().toString();
-	 @Parameters({ "enivironment" })
-	 @BeforeMethod
-    private void Initialize(String env)
-    {
-	 this.driver = DecideEnvironment(env);
-	 loginObj = new LoginPageLib(driver);
-	 homeObj = loginObj.login("ravikiran@fourthfrontier.com",MilesUtilities.DecryptPass("MTIzNDEyMzQ="));
-    }
-
-	 @AfterMethod
-	 private void CloseDriverSession(ITestResult result) throws Exception
-	 {
-		 if (result.getStatus() == ITestResult.FAILURE) 
-		 {
-			 MilesUtilities.createWorkFlowFolder(ClassName);
-			 MilesUtilities.CaptureEvidance(driver,result.getMethod().getMethodName(),result,ClassName);
-			 System.out.println(result.getMethod().getMethodName()+" Test Failed Due to the reason\n"+result.getThrowable().getMessage());
-		 }        
-		 driver.quit();
-	 }
-	
-	 
-@BeforeClass
-private void SetEvidenceDir()
-{
-	MilesUtilities.createDateBasedDirectory();
-	
-}
-	
-	@Test (description = "Purpose of this test is to Verify that User can Sync his activity and it is being displayed")
-	public void VerifyActivitySync() throws Exception
-	{			
-				
-					//homeObj.VerifyTodaysActivityIsDisplayed();
-					
-	}
-	    
-	@Test(description = "Purpose of this test is to Verify that User can Delete the activity - But here i'm not deleting it , just checking if he can do so")
-	public void deleteActivity() throws Exception
-	{	
-	    	
-			
-					homeObj.clickOnActivityButton();
-					justSleepFor(3);
-					homeObj.deleteActivity();
-					justSleepFor(3);
-					homeObj.ClickkeepButton();
-					justSleepFor(3);
-					homeObj.logout();
-					justSleepFor(3);
-	}
-	
-	@Test(description = "Purpose of this test is to Verify Training load on home page and Workout details page are same")
-	public void VerifyTrainingLoad() throws Exception
-	{	
-		WorkoutDetailsPageLib wd = new WorkoutDetailsPageLib(driver);
-		justSleepFor(3);
-		int TLHomePage = homeObj.GetTrainingLoad_ActivityLevel();
-		homeObj.clickOnGreenTick();
-		MilesUtilities.SwitchTab(1,driver);
-		justSleepFor(7);
-		Assert.assertEquals(TLHomePage, wd.GetTrainingLoadValue()) ;
-	}
-
-	
-	@Test (description = "Purpose of this test is to Verify that ECG has been synced and Green Tick mark is visble ")
-	public void VerifyECGSyncIsSuccessfull() throws Exception
-	{		
-		WorkoutDetailsPageLib workoutObj = new WorkoutDetailsPageLib(this.driver);
-				
-			//homeObj.VerifyTodaysECGIsSynced();
-			homeObj.clickOnGreenTick();
-			workoutObj.VerifyECGGraphIsDisplayed();
-	 }
-	    
-	@Test (description = "Purpose of this test is to Verify that user can Download and verify Current Day's FIT File and verifies if its valid or not based on length of file")
-	public void VerifyUserCanDownloadLatestFITFiles() throws Exception
-	{
-	    String ext = ".fit";
-	
-				MilesUtilities.deleteFiles(ext);
-				justSleepFor(6);
-				
-					homeObj.clickOnActivityButton();
-					homeObj.clickOnDownloadFIT();
-					System.out.println("FIT File option was clicked for user");
-					justSleepFor(4);
-					Assert.assertTrue(MilesUtilities.isCurrentDaysFileDownloaded(ext),"Fit File downloaded");
-				driver.close();	
-	}
-	
-	@Test (description = "Purpose of this test is to Verify that User can Edit the Activity and save it")
-	public void VerifyEditedActivity() throws Exception 
-	{
-			
-			homeObj.clickOnActivityButton();
-			justSleepFor(3);
-			homeObj.editActivity();
-			justSleepFor(3);
-			homeObj.EnterText_EditActivity();
-			justSleepFor(3);
-			homeObj.VerifyEditedTextDisplayed();
-			justSleepFor(3);
-			homeObj.logout();
-	}
-	
-	
-	@Test (description = "Purpose of this test is to Verify if Insights are displayed or not ")
-	public void VerifyInsightInfoAreDisplayed() throws Exception
-	{		
-		WorkoutDetailsPageLib wd = new WorkoutDetailsPageLib(driver);
-				
-				//	homeObj.VerifyTodaysActivityIsDisplayed();
-					homeObj.clickOnGreenTick();
-					justSleepFor(7);
-					wd.AssertInsightInfo();
-	}
-	
-	
-	@Test (description = "Purpose of this test to verify Add Report button and Section is working as expected")
-	public void VerifyAddReportSection() throws Exception
-	{		
-		Thread.sleep(3000);
-		homeObj.ClickAddReportBtn();
-		Thread.sleep(3000);
-		homeObj.VerifyAddReportScreen();
-	}
-
-	
-}
+//package com.Miles.TestScripts;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.testng.Assert;
+//import org.testng.ITestResult;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Parameters;
+//import org.testng.annotations.Test;
+//
+//import com.miles.BaseSettings.MilesSettings;
+//import com.miles.PageLibRepo.HeartHealthPageLib;
+//import com.miles.PageLibRepo.HomePageLib;
+//import com.miles.PageLibRepo.LoginPageLib;
+//import com.miles.PageLibRepo.WorkoutDetailsPageLib;
+//import com.miles.Utilities.MilesUtilities;
+//
+//public class FxUserWorkFlows extends MilesSettings 
+//{	
+//	 WebDriver driver = null ;
+//	 LoginPageLib loginObj ;
+//	 HomePageLib homeObj ;
+//	 
+//	 String ClassName = this.getClass().getSimpleName().toString();
+//	 @Parameters({ "enivironment" })
+//	 @BeforeMethod
+//    private void Initialize(String env)
+//    {
+//	 this.driver = DecideEnvironment(env);
+//	 loginObj = new LoginPageLib(driver);
+//	 homeObj = loginObj.login("ravikiran@fourthfrontier.com",MilesUtilities.DecryptPass("MTIzNDEyMzQ="));
+//    }
+//
+//	 @AfterMethod
+//	 private void CloseDriverSession(ITestResult result) throws Exception
+//	 {
+//		 if (result.getStatus() == ITestResult.FAILURE) 
+//		 {
+//			 MilesUtilities.createWorkFlowFolder(ClassName);
+//			 MilesUtilities.CaptureEvidance(driver,result.getMethod().getMethodName(),result,ClassName);
+//			 System.out.println(result.getMethod().getMethodName()+" Test Failed Due to the reason\n"+result.getThrowable().getMessage());
+//		 }        
+//		 driver.quit();
+//	 }
+//	
+//	 
+//@BeforeClass
+//private void SetEvidenceDir()
+//{
+//	MilesUtilities.createDateBasedDirectory();
+//	
+//}
+//	
+//	@Test (description = "Purpose of this test is to Verify that User can Sync his activity and it is being displayed")
+//	public void VerifyActivitySync() throws Exception
+//	{			
+//				
+//					//homeObj.VerifyTodaysActivityIsDisplayed();
+//					
+//	}
+//	    
+//	@Test(description = "Purpose of this test is to Verify that User can Delete the activity - But here i'm not deleting it , just checking if he can do so")
+//	public void deleteActivity() throws Exception
+//	{	
+//	    	
+//			
+//					homeObj.clickOnActivityButton();
+//					justSleepFor(3);
+//					homeObj.deleteActivity();
+//					justSleepFor(3);
+//					homeObj.ClickkeepButton();
+//					justSleepFor(3);
+//					homeObj.logout();
+//					justSleepFor(3);
+//	}
+//	
+//	@Test(description = "Purpose of this test is to Verify Training load on home page and Workout details page are same")
+//	public void VerifyTrainingLoad() throws Exception
+//	{	
+//		WorkoutDetailsPageLib wd = new WorkoutDetailsPageLib(driver);
+//		justSleepFor(3);
+//		int TLHomePage = homeObj.GetTrainingLoad_ActivityLevel();
+//		homeObj.clickOnGreenTick();
+//		MilesUtilities.SwitchTab(1,driver);
+//		justSleepFor(7);
+//		Assert.assertEquals(TLHomePage, wd.GetTrainingLoadValue()) ;
+//	}
+//
+//	
+//	@Test (description = "Purpose of this test is to Verify that ECG has been synced and Green Tick mark is visble ")
+//	public void VerifyECGSyncIsSuccessfull() throws Exception
+//	{		
+//		WorkoutDetailsPageLib workoutObj = new WorkoutDetailsPageLib(this.driver);
+//				
+//			//homeObj.VerifyTodaysECGIsSynced();
+//			homeObj.clickOnGreenTick();
+//			workoutObj.VerifyECGGraphIsDisplayed();
+//	 }
+//	    
+//	@Test (description = "Purpose of this test is to Verify that user can Download and verify Current Day's FIT File and verifies if its valid or not based on length of file")
+//	public void VerifyUserCanDownloadLatestFITFiles() throws Exception
+//	{
+//	    String ext = ".fit";
+//	
+//				MilesUtilities.deleteFiles(ext);
+//				justSleepFor(6);
+//				
+//					homeObj.clickOnActivityButton();
+//					homeObj.clickOnDownloadFIT();
+//					System.out.println("FIT File option was clicked for user");
+//					justSleepFor(4);
+//					Assert.assertTrue(MilesUtilities.isCurrentDaysFileDownloaded(ext),"Fit File downloaded");
+//				driver.close();	
+//	}
+//	
+//	@Test (description = "Purpose of this test is to Verify that User can Edit the Activity and save it")
+//	public void VerifyEditedActivity() throws Exception 
+//	{
+//			
+//			homeObj.clickOnActivityButton();
+//			justSleepFor(3);
+//			homeObj.editActivity();
+//			justSleepFor(3);
+//			homeObj.EnterText_EditActivity();
+//			justSleepFor(3);
+//			homeObj.VerifyEditedTextDisplayed();
+//			justSleepFor(3);
+//			homeObj.logout();
+//	}
+//	
+//	
+//	@Test (description = "Purpose of this test is to Verify if Insights are displayed or not ")
+//	public void VerifyInsightInfoAreDisplayed() throws Exception
+//	{		
+//		WorkoutDetailsPageLib wd = new WorkoutDetailsPageLib(driver);
+//				
+//				//	homeObj.VerifyTodaysActivityIsDisplayed();
+//					homeObj.clickOnGreenTick();
+//					justSleepFor(7);
+//					wd.AssertInsightInfo();
+//	}
+//	
+//	
+//	@Test (description = "Purpose of this test to verify Add Report button and Section is working as expected")
+//	public void VerifyAddReportSection() throws Exception
+//	{		
+//		Thread.sleep(3000);
+//		homeObj.ClickAddReportBtn();
+//		Thread.sleep(3000);
+//		homeObj.VerifyAddReportScreen();
+//	}
+//
+//	
+//}
