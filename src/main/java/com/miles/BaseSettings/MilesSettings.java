d960f5bf47565e871e88f19868258d5b32b2d5f6
Manoj <manoj.hr@mileseducation.com>
2024-11-05 17:05:49 +0530

    ijk

--- src/main/java/com/miles/BaseSettings/MilesSettings.java
+++ src/main/java/com/miles/BaseSettings/MilesSettings.java
@@ -163,7 +163,7 @@ public static void getChromeVersion() throws IOException
 				options.addArguments("--remote-allow-origins=*");
 				options.addArguments("--no-sandbox");
 				options.addArguments("start-maximized"); // open Browser in maximized mode
-				options.addArguments("--window-size=1920x1080"); //1920x1080 
+				options.addArguments("--window-size=2560,1440"); //1920x1080 
 				options.addArguments("disable-infobars"); // disabling infobars
 				options.addArguments("--disable-extensions"); // disabling extensions
 				options.addArguments("--headless"); // to run in headless mode on ec2 os only
