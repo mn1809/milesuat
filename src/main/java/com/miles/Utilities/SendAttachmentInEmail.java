5e68e7e1bcc0177b98f0b7d255ca2141e9723d3a
Manoj <manoj.hr@mileseducation.com>
2024-11-06 17:12:30 +0530

    Mail Check

--- src/main/java/com/miles/Utilities/SendAttachmentInEmail.java
+++ src/main/java/com/miles/Utilities/SendAttachmentInEmail.java
@@ -39,7 +39,7 @@ public class SendAttachmentInEmail
       props.put("mail.smtp.auth", "true");
       props.put("mail.smtp.starttls.enable", "true");
       props.put("mail.smtp.host", host);
-      props.put("mail.smtp.port", "587");
+      props.put("mail.smtp.port", "465");
       //props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
       // Get the Session object.
