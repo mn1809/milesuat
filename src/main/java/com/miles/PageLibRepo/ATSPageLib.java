a77f25bbc0075299ba8d52814e157c5888b7d5f6
Manoj <manoj.hr@mileseducation.com>
2024-11-25 18:04:49 +0530

    r

--- src/main/java/com/miles/PageLibRepo/ATSPageLib.java
+++ src/main/java/com/miles/PageLibRepo/ATSPageLib.java
@@ -1163,7 +1163,7 @@ Thread.sleep(2000);
 	    robot3.keyPress(KeyEvent.VK_ENTER);
 	    robot3.keyRelease(KeyEvent.VK_ENTER);
 	    Thread.sleep(10000);
-	    driver.findElement(By.xpath("//*[contains(@name, 'action_selected_enrolled_university175757')]")).click();  
+	    driver.findElement(By.xpath("//*[contains(@name, 'action_selected_enrolled_university')]")).click();  
 	    Thread.sleep(1500);
 	    driver.findElements(By.xpath("//*[contains(@class, 'btn btn-primary')]")).get(2).click();
 	    Thread.sleep(7000);
