1162b0028278b5dd90938e7dd19452cc4434a241
Manoj <manoj.hr@mileseducation.com>
2024-11-29 10:20:55 +0530

    TEST1

--- src/test/java/com/Miles/SanityScripts/Miles_ATS_FlowAsAdmin.java
+++ src/test/java/com/Miles/SanityScripts/Miles_ATS_FlowAsAdmin.java
@@ -509,7 +509,7 @@ public void VerifyU7PlusExpertSeesionBooked() throws InterruptedException
 
 }
 
-@Test (priority = 19, description = "U7+ Expert Session Booked Eligible Candidate")
+//@Test (priority = 19, description = "U7+ Expert Session Booked Eligible Candidate")
 
 public void VerifyU7PlusExpertSeesionEligibleCandidate() throws InterruptedException
 {
@@ -520,7 +520,7 @@ public void VerifyU7PlusExpertSeesionEligibleCandidate() throws InterruptedExcep
 	ATS.TabsbuttonOnU7PlusEnrolled();
 	Thread.sleep(3000);
 
-	ATS.RecommendationCompleted();
+	//ATS.RecommendationCompleted();
 	Thread.sleep(3000);
 	ATS.U7PLusEligibleTheCandidate();
 	
@@ -528,7 +528,7 @@ public void VerifyU7PlusExpertSeesionEligibleCandidate() throws InterruptedExcep
 
 
 
-//@Test (priority = 20, description = "U7+ Recommend University To Candidate")
+@Test (priority = 20, description = "U7+ Recommend University To Candidate")
 public void VerifyU7PlusRecommendUniversity() throws InterruptedException, AWTException
 {
 	ATSPageLib ATS = new ATSPageLib(driver);
